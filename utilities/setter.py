70d2feffec4c4bfbba2cdc2bad754fd4d1372cd8
Simon-creator-jara <70153636+Simon-creator-jara@users.noreply.github.com>
2024-04-04 11:54:08 -0500

    Fixing workflow

--- utilities/setter.py
+++ utilities/setter.py
@@ -4,9 +4,8 @@ from base64 import b64decode
 def main():
     key = os.environ.get('SERVICE-ACCOUNT_KEY')
     with open('path.json','w') as json_file:
-        json_file.write(b64decode(key.decode()))
+        json_file.write(b64decode(key).decode())
     print(os.path.realpath('path.json'))
 
 if __name__ == '__main__':
     main()
-    
